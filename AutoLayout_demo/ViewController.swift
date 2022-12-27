5f4a7185b5a9af60dd5208657fab11ed6a84b554
Mustofa Mahmud <mustofa.mahmud@bjitgroup.com>
2022-12-27 15:14:40 +0600

    test

--- AutoLayout_demo/ViewController.swift
+++ AutoLayout_demo/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let divisions = ["Dhaka", "Khulna", "Rajshahi", "Borisal", "Sylhet"]
+    
+    @IBOutlet weak var divisionPicker: UIPickerView!
+    
+    @IBOutlet weak var lBselectDivision: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return divisions.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return divisions[row]
+    }
 
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("Selected \(divisions[row])")
+        lBselectDivision.text = divisions[row]
+    }
 
 }
 
